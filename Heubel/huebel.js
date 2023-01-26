caba35164e1409449ee5eb8bbbd93b4ae365259b
ChristopherV3 <84456226+223cvalente@users.noreply.github.com>
2023-01-26 14:24:59 +0000

    dd

--- Heubel/huebel.js
+++ Heubel/huebel.js
@@ -1,10 +1,10 @@
+
 const arrowTop = document.querySelector('.arrow-top');
 
 arrowTop.addEventListener('click', function(event) {
   event.preventDefault();
   window.scrollTo({ top: 0, behavior: 'smooth' });
 });
-
 
 
 
